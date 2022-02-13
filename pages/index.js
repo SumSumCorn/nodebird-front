728969d645a009802f1457df717e30b5c0fa38ac
SumSumCorn <a01035587180@gmail.com>
2022-02-13 15:10:07 +0900

    init

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <AppLayout>
+            <div>hello world</div>
+        </AppLayout>
+    );
+}
+
+import AppLayout from "../components/AppLayout";
+
+export default Home;
